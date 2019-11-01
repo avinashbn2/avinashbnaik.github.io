a24614cd60f52d59969a63cbf7acba965d6ba538
avi <avinash.naik@lendfoundry.com>
2019-11-01 22:08:45 +0530

    Updates

--- src/components/Container.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components"
-const Container = styled.div`
-  margin: 0 auto;
-  margin-bottom: 40px;
-`
-export default Container
---
+++ styles-f985cf64257d6ca0f7f9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"sg+I":function(n,w,o){}}]);
+//# sourceMappingURL=styles-f985cf64257d6ca0f7f9.js.map
